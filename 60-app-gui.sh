e02ea826113229849c65e353eb5028fca568bcce
Izaw <IzawGithub@pm.me>
2025-09-26 15:53:24 +0200

    feat(Gui): Add GUI apps files

--- 60-app-gui.sh
+++ 60-app-gui.sh
@@ -15,3 +15,35 @@ AddPackage --foreign vscodium-bin # Binary releases of VS Code without MS brandi
 AddPackage --foreign vscodium-bin-marketplace # Enable vscode marketplace in vscodium-bin
 AddPackage --foreign vscodium-xdg-dir-patch # Patch for VSCodium to store data in ~/.local/share/codium
 
+# ProtonVPN
+CreateDir /etc/openvpn/client 750 openvpn network
+CreateDir /etc/openvpn/server 750 openvpn network
+
+CopyFile /home/izaw/.config/alacritty/alacritty.toml '' izaw izaw
+CopyFile /home/izaw/.config/alacritty/keybindings.toml '' izaw izaw
+CopyFile /home/izaw/.config/alacritty/themes/README.md '' izaw izaw
+CopyFile /home/izaw/.config/alacritty/themes/catppuccin_frappe.toml '' izaw izaw
+CopyFile /home/izaw/.config/alacritty/themes/catppuccin_latte.toml '' izaw izaw
+CopyFile /home/izaw/.config/alacritty/themes/catppuccin_macchiato.toml '' izaw izaw
+CopyFile /home/izaw/.config/alacritty/themes/catppuccin_mocha.toml '' izaw izaw
+CopyFile /home/izaw/.config/VSCodium/User/keybindings.json '' izaw izaw
+CopyFile /home/izaw/.config/VSCodium/User/settings.json '' izaw izaw
+CopyFile /home/izaw/.local/share/codium/extensions/extensions.json '' izaw izaw
+CopyFile /home/izaw/.config/zed/keymap.json '' izaw izaw
+CopyFile /home/izaw/.config/zed/settings.json '' izaw izaw
+
+SetFileProperty /home/izaw/.config/alacritty/themes group izaw
+SetFileProperty /home/izaw/.config/alacritty/themes owner izaw
+SetFileProperty /home/izaw/.config/alacritty group izaw
+SetFileProperty /home/izaw/.config/alacritty owner izaw
+SetFileProperty /home/izaw/.config/VSCodium/User group izaw
+SetFileProperty /home/izaw/.config/VSCodium/User owner izaw
+SetFileProperty /home/izaw/.config/VSCodium group izaw
+SetFileProperty /home/izaw/.config/VSCodium mode 700
+SetFileProperty /home/izaw/.config/VSCodium owner izaw
+SetFileProperty /home/izaw/.local/share/codium/extensions group izaw
+SetFileProperty /home/izaw/.local/share/codium/extensions owner izaw
+SetFileProperty /home/izaw/.local/share/codium group izaw
+SetFileProperty /home/izaw/.local/share/codium owner izaw
+SetFileProperty /home/izaw/.config/zed group izaw
+SetFileProperty /home/izaw/.config/zed owner izaw
---
+++ files/home/izaw/.config/alacritty/themes/README.md
@@ -0,0 +1,4 @@
+Repo is https://github.com/alacritty/alacritty-theme
+
+Clone it if you want to sync.
+It's not cloned by default because it's quite large for aconfmgr.
