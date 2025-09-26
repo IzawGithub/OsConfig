a9827796b99ee437716334fe722435d92d425065
Izaw <IzawGithub@pm.me>
2025-09-26 15:53:24 +0200

    feat(Gui): Add GUI apps packages

---
+++ 60-app-gui.sh
@@ -0,0 +1,17 @@
+AddPackage alacritty # A cross-platform, GPU-accelerated terminal emulator
+AddPackage discord # All-in-one voice and text chat for gamers
+AddPackage gimp # GNU Image Manipulation Program
+AddPackage libreoffice-still # LibreOffice maintenance branch
+AddPackage mpv # a free, open source, and cross-platform media player
+AddPackage nicotine+ # Soulseek music-sharing client, written in python
+AddPackage obs-studio # Free, open source software for live streaming and recording
+AddPackage proton-vpn-gtk-app # ProtonVPN GTK app, Maintained by Community
+AddPackage steam # Valve's digital software delivery system
+AddPackage zed # A high-performance, multiplayer code editor from the creators of Atom and Tree-sitter
+
+AddPackage --foreign freetube-bin # An open source desktop YouTube player built with privacy in mind.
+AddPackage --foreign proton-mail-bin # Proton official desktop application for Proton Mail and Proton Calendar
+AddPackage --foreign vscodium-bin # Binary releases of VS Code without MS branding/telemetry/licensing.
+AddPackage --foreign vscodium-bin-marketplace # Enable vscode marketplace in vscodium-bin
+AddPackage --foreign vscodium-xdg-dir-patch # Patch for VSCodium to store data in ~/.local/share/codium
+
