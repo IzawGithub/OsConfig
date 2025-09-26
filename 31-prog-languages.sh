3ea7b27b871f5cedb58902e7de7ce25161d2621f
Izaw <IzawGithub@pm.me>
2025-09-26 15:53:24 +0200

    feat(Prog): Add programming languages files

--- 31-prog-languages.sh
+++ 31-prog-languages.sh
@@ -1,3 +1,7 @@
 AddPackage clang # C language family frontend for LLVM
 AddPackage python # The Python programming language
 
+CopyFile /home/izaw/.config/npm/npmrc '' izaw izaw
+
+SetFileProperty /home/izaw/.config/npm group izaw
+SetFileProperty /home/izaw/.config/npm owner izaw
