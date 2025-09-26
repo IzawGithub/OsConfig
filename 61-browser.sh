47d564d667e006e3e32a3ebdf3b46ef479e04bec
Izaw <IzawGithub@pm.me>
2025-09-26 15:53:24 +0200

    feat(Browser): Add browsers packages

---
+++ 61-browser.sh
@@ -0,0 +1,5 @@
+AddPackage chromium # A web browser built for speed, simplicity, and security
+AddPackage torbrowser-launcher # Securely and easily download, verify, install, and launch Tor Browser in Linux
+
+AddPackage --foreign librewolf-bin # Community-maintained fork of Firefox, focused on privacy, security and freedom.
+
