5a6ebfcc79d8abb633f32c48b9a415cc400237b9
Izaw <IzawGithub@pm.me>
2025-09-26 15:53:24 +0200

    feat(Networking): Add networking files

--- 21-networking.sh
+++ 21-networking.sh
@@ -1,2 +1,5 @@
 AddPackage networkmanager # Network connection manager and user applications
 
+CreateLink /etc/systemd/system/dbus-org.freedesktop.nm-dispatcher.service /usr/lib/systemd/system/NetworkManager-dispatcher.service
+CreateLink /etc/systemd/system/multi-user.target.wants/NetworkManager.service /usr/lib/systemd/system/NetworkManager.service
+CreateLink /etc/systemd/system/network-online.target.wants/NetworkManager-wait-online.service /usr/lib/systemd/system/NetworkManager-wait-online.service
