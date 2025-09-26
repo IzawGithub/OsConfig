dbaffedde0de186a0f6f55b0958b6180cdc50b70
Izaw <IzawGithub@pm.me>
2025-09-26 15:53:24 +0200

    feat(CLI): Add CLI apps packages

---
+++ 30-app-cli.sh
@@ -0,0 +1,25 @@
+AddPackage 7zip # File archiver for extremely high compression
+AddPackage asciinema # Record and share terminal sessions
+AddPackage bat # Cat clone with syntax highlighting and git integration
+AddPackage bottom # A graphical process/system monitor
+AddPackage cloc # Count lines of code
+AddPackage dust # A more intuitive version of du in rust
+AddPackage eza # A modern replacement for ls (community fork of exa)
+AddPackage fd # Simple, fast and user-friendly alternative to find
+AddPackage fzf # Command-line fuzzy finder
+AddPackage hyperfine # A command-line benchmarking tool
+AddPackage jq # Command-line JSON processor
+AddPackage just # A handy way to save and run project-specific commands
+AddPackage miniserve # Tool to serve files via HTTP
+AddPackage ripgrep # A search tool that combines the usability of ag with the raw speed of grep
+AddPackage sudo-rs # A memory-safe implementation of sudo and su
+AddPackage tealdeer # A fast tldr client in Rust
+AddPackage uutils-coreutils # Cross-platform Rust rewrite of the GNU coreutils
+AddPackage wget # Network utility to retrieve files from the web
+AddPackage zellij # A terminal multiplexer
+
+AddPackage --foreign dprint-bin # Pluggable and configurable code formatting platform
+AddPackage --foreign kibi # A tiny text editor, written in Rust
+AddPackage --foreign yay # Yet another yogurt. Pacman wrapper and AUR helper written in go.
+AddPackage --foreign yay-debug # Detached debugging symbols for yay
+
