0c9691c8c0a609fa3d825470f94fd0cd7bd476f8
Izaw <IzawGithub@pm.me>
2025-09-26 15:53:24 +0200

    feat(Bluetooth): Add bluetooth files

--- 23-bluetooth.sh
+++ 23-bluetooth.sh
@@ -1,3 +1,5 @@
 AddPackage bluez # Daemons for the bluetooth protocol stack
 AddPackage bluez-utils # Development and debugging utilities for the bluetooth protocol stack
 
+CreateLink /etc/systemd/system/bluetooth.target.wants/bluetooth.service /usr/lib/systemd/system/bluetooth.service
+CreateLink /etc/systemd/system/dbus-org.bluez.service /usr/lib/systemd/system/bluetooth.service
