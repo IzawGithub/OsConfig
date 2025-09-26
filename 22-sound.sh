77b37c36eeeb4bed960ebb36e5fb237696e04348
Izaw <IzawGithub@pm.me>
2025-09-26 15:53:24 +0200

    feat(Sound): Add sound files

--- 22-sound.sh
+++ 22-sound.sh
@@ -6,3 +6,13 @@ AddPackage pipewire-jack # Low-latency audio/video router and processor - JACK r
 AddPackage pipewire-pulse # Low-latency audio/video router and processor - PulseAudio replacement
 AddPackage wireplumber # Session / policy manager implementation for PipeWire
 
+CreateLink /etc/systemd/user/pipewire-session-manager.service /usr/lib/systemd/user/wireplumber.service
+CreateLink /etc/systemd/user/pipewire.service.wants/wireplumber.service /usr/lib/systemd/user/wireplumber.service
+CreateLink /etc/systemd/user/sockets.target.wants/pipewire-pulse.socket /usr/lib/systemd/user/pipewire-pulse.socket
+CreateLink /etc/systemd/user/sockets.target.wants/pipewire.socket /usr/lib/systemd/user/pipewire.socket
+
+CopyFile /home/izaw/.config/pulse/client.conf '' izaw izaw
+
+SetFileProperty /home/izaw/.config/pulse group izaw
+SetFileProperty /home/izaw/.config/pulse mode 700
+SetFileProperty /home/izaw/.config/pulse owner izaw
