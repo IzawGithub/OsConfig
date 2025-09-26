ec1bc306ad5d4cfa1e09a150d991a97871f75108
Izaw <IzawGithub@pm.me>
2025-09-26 15:53:24 +0200

    feat(Sound): Add sound packages

---
+++ 22-sound.sh
@@ -0,0 +1,8 @@
+AddPackage gst-plugin-pipewire # Multimedia graph framework - pipewire plugin
+AddPackage libpulse # A featureful, general-purpose sound server (client library)
+AddPackage pipewire # Low-latency audio/video router and processor
+AddPackage pipewire-alsa # Low-latency audio/video router and processor - ALSA configuration
+AddPackage pipewire-jack # Low-latency audio/video router and processor - JACK replacement
+AddPackage pipewire-pulse # Low-latency audio/video router and processor - PulseAudio replacement
+AddPackage wireplumber # Session / policy manager implementation for PipeWire
+
