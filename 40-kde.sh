d95b3ca022662c126070f0fa1a2f4ff412616a96
Izaw <IzawGithub@pm.me>
2025-09-26 15:53:24 +0200

    feat(KDE): Add KDE packages

---
+++ 40-kde.sh
@@ -0,0 +1,15 @@
+AddPackage ark # Archiving Tool
+AddPackage discover # KDE and Plasma resources management GUI
+AddPackage dolphin # KDE File Manager
+AddPackage dolphin-plugins # Extra Dolphin plugins
+AddPackage elisa # A simple music player aiming to provide a nice experience for its users
+AddPackage filelight # View disk usage information
+AddPackage kdeconnect # Adds communication between KDE and your smartphone
+AddPackage kdenlive # A non-linear video editor for Linux using the MLT video framework
+AddPackage kleopatra # Certificate Manager and Unified Crypto GUI
+AddPackage libplasma # Plasma library and runtime components
+AddPackage papirus-icon-theme # Papirus icon theme
+AddPackage partitionmanager # A KDE utility that allows you to manage disks, partitions, and file systems
+AddPackage plasma-desktop # KDE Plasma Desktop
+AddPackage plasma-meta # Meta package to install KDE Plasma
+
