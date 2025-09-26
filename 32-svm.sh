bfa1f9ffb60750d0efaef7c715a8f8b26d1ec42f
Izaw <IzawGithub@pm.me>
2025-09-26 15:53:24 +0200

    feat(SVM): Add source version control files

--- 32-svm.sh
+++ 32-svm.sh
@@ -5,3 +5,20 @@ AddPackage jujutsu # Git-compatible VCS that is both simple and powerful
 
 AddPackage --foreign jjui-bin # Text User Interface (TUI) designed for interacting with the Jujutsu version control system.
 
+CopyFile /home/izaw/.config/git/config '' izaw izaw
+CopyFile /home/izaw/.config/jj/config.toml '' izaw izaw
+CopyFile /home/izaw/.config/jjui/config.toml '' izaw izaw
+CopyFile /home/izaw/.config/jjui/themes/README.md '' izaw izaw
+CopyFile /home/izaw/.config/jjui/themes/base24-catppuccin-frappe.toml '' izaw izaw
+CopyFile /home/izaw/.config/jjui/themes/base24-catppuccin-latte.toml '' izaw izaw
+CopyFile /home/izaw/.config/jjui/themes/base24-catppuccin-macchiato.toml '' izaw izaw
+CopyFile /home/izaw/.config/jjui/themes/base24-catppuccin-mocha.toml '' izaw izaw
+
+SetFileProperty /home/izaw/.config/git group izaw
+SetFileProperty /home/izaw/.config/git owner izaw
+SetFileProperty /home/izaw/.config/jj group izaw
+SetFileProperty /home/izaw/.config/jj owner izaw
+SetFileProperty /home/izaw/.config/jjui/themes group izaw
+SetFileProperty /home/izaw/.config/jjui/themes owner izaw
+SetFileProperty /home/izaw/.config/jjui group izaw
+SetFileProperty /home/izaw/.config/jjui owner izaw
