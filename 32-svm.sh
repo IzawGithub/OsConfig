dc2719340ef6a8e6d86208a2a4569512fccf1cbb
Izaw <IzawGithub@pm.me>
2025-09-26 15:53:24 +0200

    feat(SVM): Add source version control packages

---
+++ 32-svm.sh
@@ -0,0 +1,7 @@
+AddPackage git # the fast distributed version control system
+AddPackage git-cliff # A highly customizable changelog generator
+AddPackage git-delta # Syntax-highlighting pager for git and diff output
+AddPackage jujutsu # Git-compatible VCS that is both simple and powerful
+
+AddPackage --foreign jjui-bin # Text User Interface (TUI) designed for interacting with the Jujutsu version control system.
+
