50042d6de3a3a00ed3f5033a310ab4f2ab781119
Izaw <IzawGithub@pm.me>
2025-09-26 15:53:24 +0200

    feat(Container): Add container packages

---
+++ 51-container.sh
@@ -0,0 +1,3 @@
+AddPackage podman # Tool and library for running OCI-based containers in pods
+AddPackage podman-compose # A script to run docker-compose.yml using podman
+AddPackage podman-desktop # Manage Podman and other container engines from a single UI and tray.
