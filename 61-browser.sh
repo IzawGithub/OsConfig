ac187391db99dfef38b5842ddd92e12a8195ebf1
Izaw <IzawGithub@pm.me>
2025-09-26 15:53:24 +0200

    feat(Browser): Add browsers files

--- 61-browser.sh
+++ 61-browser.sh
@@ -3,3 +3,14 @@ AddPackage torbrowser-launcher # Securely and easily download, verify, install, 
 
 AddPackage --foreign librewolf-bin # Community-maintained fork of Firefox, focused on privacy, security and freedom.
 
+CopyFile /home/izaw/.librewolf/installs.ini '' izaw izaw
+CopyFile /home/izaw/.librewolf/x1hlwolm.default-default/addons.json '' izaw izaw
+CopyFile /home/izaw/.librewolf/x1hlwolm.default-default/extension-preferences.json '' izaw izaw
+CopyFile /home/izaw/.librewolf/x1hlwolm.default-default/extensions.json '' izaw izaw
+
+SetFileProperty /home/izaw/.librewolf/x1hlwolm.default-default group izaw
+SetFileProperty /home/izaw/.librewolf/x1hlwolm.default-default mode 700
+SetFileProperty /home/izaw/.librewolf/x1hlwolm.default-default owner izaw
+SetFileProperty /home/izaw/.librewolf group izaw
+SetFileProperty /home/izaw/.librewolf mode 700
+SetFileProperty /home/izaw/.librewolf owner izaw
