343a80a5269e724886ec49bd0b56c951e4f9a0af
Izaw <IzawGithub@pm.me>
2025-09-26 15:53:24 +0200

    feat(Base): Add base packages

---
+++ 20-base.sh
@@ -0,0 +1,10 @@
+AddPackage base # Minimal package set to define a basic Arch Linux installation
+AddPackage base-devel # Basic tools to build Arch Linux packages
+AddPackage grub # GNU GRand Unified Bootloader (2)
+AddPackage linux-firmware # Firmware files for Linux - Default set
+AddPackage linux-zen # The Linux ZEN kernel and modules
+AddPackage man-db # A utility for reading man pages
+AddPackage zram-generator # Systemd unit generator for zram devices
+
+AddPackage --foreign aconfmgr-git # A configuration manager for Arch Linux
+
