7b41bb5362121801b5898f2ab54cce2976ddfb18
Izaw <IzawGithub@pm.me>
2025-09-26 15:53:24 +0200

    feat(Virt): Add virtualization packages

---
+++ 50-virtualization.sh
@@ -0,0 +1,4 @@
+AddPackage libvirt # API for controlling virtualization engines (openvz,kvm,qemu,virtualbox,xen,etc)
+AddPackage qemu-desktop # A QEMU setup for desktop environments
+AddPackage qemu-emulators-full # All QEMU user mode and system emulators
+AddPackage virt-manager # Desktop user interface for managing virtual machines
