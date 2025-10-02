9cb2063febce72ac088de43d7305b8ec2c6caaac
Izaw <IzawGithub@pm.me>
2025-10-02 14:24:06 +0200

    feat(Font): Add fonts to fix unicode rendering

--- 62-fonts.sh
+++ 62-fonts.sh
@@ -1,1 +1,7 @@
+AddPackage noto-fonts-cjk # Google Noto CJK fonts
+AddPackage noto-fonts-extra # Google Noto TTF fonts - additional variants
+AddPackage ttf-dejavu # Font family based on the Bitstream Vera Fonts with a wider range of characters
+AddPackage ttf-fira-sans # Mozilla's sans-serif typeface designed for Firefox OS
+AddPackage ttf-firacode-nerd # Patched font Fira (Fura) Code from nerd fonts library
 AddPackage ttf-jetbrains-mono-nerd # Patched font JetBrains Mono from nerd fonts library
+AddPackage ttf-roboto # Google's signature family of fonts
