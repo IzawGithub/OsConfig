6d17ce2812de98d6ee99e0207a42e5fa3dccbf43
Izaw <IzawGithub@pm.me>
2025-10-02 14:24:06 +0200

    feat(Filesystem): Add `compsize`

--- 25-filesystem.sh
+++ 25-filesystem.sh
@@ -1,2 +1,3 @@
 AddPackage btrfs-progs # Btrfs filesystem utilities
+AddPackage compsize # Calculate compression ratio of a set of files on Btrfs
 AddPackage snapper # A tool for managing BTRFS and LVM snapshots
