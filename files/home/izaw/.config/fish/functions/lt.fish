e9dd54186a360511b97645ddd105cd8d91a93f13
Izaw <IzawGithub@pm.me>
2025-10-20 10:56:06 +0200

    feat(Shell): Fisher plugin update

--- files/home/izaw/.config/fish/functions/lt.fish
+++ files/home/izaw/.config/fish/functions/lt.fish
@@ -1,4 +1,3 @@
 function lt --wraps='eza $EXA_STANDARD_OPTIONS $EXA_LT_OPTIONS' --description 'alias lt eza $EXA_STANDARD_OPTIONS $EXA_LT_OPTIONS'
-  eza $EXA_STANDARD_OPTIONS $EXA_LT_OPTIONS $argv
-        
+    eza $EXA_STANDARD_OPTIONS $EXA_LT_OPTIONS $argv
 end
